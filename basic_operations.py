1a4523c5d955f38ea8c915ed5743d0840d1da91a
laurentius-mbugua <laurentiusmbugua11@gmail.com>
2024-09-13 12:30:32 +0300

    Simple maths

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+
+print("addition of", number1, "and", number2, "is", addition)
+print("subtraction of", number1, "and", number2, "is", subtraction)
+print("product of", number1, "and", number2, "is", product)
