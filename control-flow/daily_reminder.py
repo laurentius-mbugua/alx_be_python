8704702de6efb240ca880e59591d53722a6ea428
laurentius-mbugua <laurentiusmbugua11@gmail.com>
2024-09-20 13:46:05 +0300

    minor bugs

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,19 +11,19 @@ while task:
     match priority:
         case "high":
             if time_bound =="yes":
-                reminder = f"Reminder: {task} is a high priority task that requires immediate attention today!"
+                reminder = f":{task} is a high priority task that requires immediate attention today!"
             else:
-                reminder = f"Reminder: {task} is a high priority task. Consider completing it when you have free time"
+                reminder = f":{task} is a high priority task. Consider completing it when you have free time"
         case "medium":
             if time_bound =="yes":
-                reminder =f"Reminder: {task} is a medium priority task that requires immediate attention today!"
+                reminder =f":{task} is a medium priority task that requires immediate attention today!"
             else:
-                reminder =f"Reminder: {task} is a medium priority task. Consider completing it when you have free time"
+                reminder =f":{task} is a medium priority task. Consider completing it when you have free time"
         case "low":
             if time_bound =="yes":
-                reminder = f"Reminder {task} is a low priority task that requires immediate attention today!"
+                reminder = f":{task} is a low priority task that requires immediate attention today!"
             else:
-                reminder = f"Reminder {task} is a low priority task. Consider completing it when you have free time"
+                reminder = f":{task} is a low priority task. Consider completing it when you have free time"
 
-    print(reminder)
+    print(f"Reminder: {reminder}")
     break
